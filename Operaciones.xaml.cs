bd2391cd950a77ae418422a18fb4ee58dff1c5f5
Ronald Zenón Cuevas Machaca <rzcuevasm@gmail.com>
2024-10-25 19:03:43 -0500

    Lo del miercoles

---
+++ Operaciones.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfIntroduccion
+{
+    /// <summary>
+    /// Lógica de interacción para Operaciones.xaml
+    /// </summary>
+    public partial class Operaciones : Window
+    {
+        public Operaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Click(object sender, RoutedEventArgs e)
+        {
+            lblSuma.Content = Convert.ToInt32(txtValor1.Text) + Convert.ToInt32(txtValor2.Text);
+        }
+
+        private void btnRestar_Click(object sender, RoutedEventArgs e)
+        {
+            lblSuma.Content = Convert.ToInt32(txtValor1.Text) - Convert.ToInt32(txtValor2.Text);
+        }
+
+        private void btnMultiplicar_Click(object sender, RoutedEventArgs e)
+        {
+            lblSuma.Content = Convert.ToInt32(txtValor1.Text) * Convert.ToInt32(txtValor2.Text);
+        }
+    }
+}
---
+++ RegistroProducto.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfIntroduccion
+{
+    /// <summary>
+    /// Lógica de interacción para RegistroProducto.xaml
+    /// </summary>
+    public partial class RegistroProducto : Window
+    {
+        List<Producto> productos = new List<Producto>();
+        public RegistroProducto()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            productos.Add(new Producto { Nombre=txtNombre.Text, Precio=Convert.ToInt32(txtPrecio.Text), Descripcion=txtDescripcion.Text});
+
+            this.dgProductos.ItemsSource = productos;
+
+            dgProductos.Items.Refresh();
+
+            Limpiar();
+        }
+
+        private void Limpiar()
+        {
+            txtNombre.Text = "";
+            txtPrecio.Text = "";
+            txtDescripcion.Text = "";
+        }
+
+    }
+
+}
